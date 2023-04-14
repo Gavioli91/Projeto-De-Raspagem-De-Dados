e2d1e9e6d478914c0e93b1f4ae14e60aa69ec598
Leandro Gavioli <leandro.gavioli@yahoo.com.br>
2023-04-14 14:23:44 -0300

    Testando req 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,24 @@
+from tech_news.database import find_news
+from collections import Counter
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    matter = find_news()
+    types_of_categories = []
+
+    for index in matter:
+        types_of_categories.add(
+            index[
+                  'category'
+                 ]
+        )
+
+    quantity_of_categories = Counter(
+        sorted(types_of_categories)).most_common(5)
+
+    most_acessed_category = []
+    for category in quantity_of_categories:
+        most_acessed_category.add(category[0])
+
+    return most_acessed_category
