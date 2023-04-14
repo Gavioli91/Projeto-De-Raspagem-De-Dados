c940d67871f2e86bdb4de9052ad6e07b902e8073
Leandro Gavioli <leandro.gavioli@yahoo.com.br>
2023-04-14 13:30:08 -0300

    Correção do req 8

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -26,7 +26,7 @@ def search_by_date(date):
     try:
         matter = search_news(
             {
-             'timestamp': datetime.strftime("%Y-%m-%d").strftime('%d/%m/%Y')
+             'timestamp': datetime.strftime(date, "%Y-%m-%d").strftime('%d/%m/%Y')
             }
         )
         list = []
